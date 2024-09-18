d7153e2f800ca1baec42638ffc6aab9336d9455e
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-18 22:33:14 +0000

    Implemented the Article List Page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -22,7 +22,7 @@ const routes = [{
     path: '/articles',
     element: <ArticlesListPage />
   }, {
-    path: '/articles/individual',
+    path: '/articles/:name', // -> /articles/learn-react
     element: <ArticlePage />
   }]
 }]
--- front-end/src/pages/ArticlePage.jsx
+++ front-end/src/pages/ArticlePage.jsx
@@ -1,5 +1,15 @@
+import { useParams } from 'react-router-dom';
+import articles from '../article-content';
+
 export default function ArticlePage() {
+  const { name } = useParams();
+
+  const article = articles.find(a => a.name === name);
+
   return (
-    <h1>This is the Article Page!</h1>
+    <>
+    <h1>{article.title}</h1>
+    {article.content.map(p => <p key={p}>{p}</p>)}
+    </>
   );
 }
