deed778ab7ff3d62cd35f2596680521973b26b87
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-18 21:55:26 +0000

    Added the rest of the pages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,32 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+
+const routes = [{
+  path: '/',
+  element: <HomePage />
+}, {
+  path: '/about',
+  element: <AboutPage />
+}, {
+  path: '/articles',
+  element: <ArticlesListPage />
+}, {
+  path: '/articles/individual',
+  element: <ArticlePage />
+}]
+
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
-    <HomePage />
+    <RouterProvider router={router} />
   );
 }
 
---
+++ front-end/src/pages/ArticlePage.jsx
@@ -0,0 +1,5 @@
+export default function ArticlePage() {
+  return (
+    <h1>This is the Article Page!</h1>
+  );
+}
