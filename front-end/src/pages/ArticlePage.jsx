22e9a838bfb5aba6f66498543fd10fa4cd0c97a8
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-19 21:33:21 +0000

    Used the useLoaderData hook

--- front-end/src/pages/ArticlePage.jsx
+++ front-end/src/pages/ArticlePage.jsx
@@ -1,14 +1,16 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useLoaderData } from 'react-router-dom';
 import articles from '../article-content';
 
 export default function ArticlePage() {
   const { name } = useParams();
+  const { upvotes, comments } = useLoaderData();
 
   const article = articles.find(a => a.name === name);
 
   return (
     <>
     <h1>{article.title}</h1>
+    <p>This article has {upvotes} upvotes!</p>
     {article.content.map(p => <p key={p}>{p}</p>)}
     </>
   );
--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://miniature-trout-gxv9wvv5r65cwjq-8000.app.github.dev',
+        changeOrigin: true,
+      }
+    }
+  }
 })
