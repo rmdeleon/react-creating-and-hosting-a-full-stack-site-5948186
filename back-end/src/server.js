5ca0891cd72f9c560873b790966dc7985e7988a1
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-19 18:19:48 +0000

    Added a POST endpoint with request body

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,9 +2,15 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/hello', function(req, res) {
-  res.send('Hello!');
+  res.send('Hello from a GET endpoint!');
 });
+
+app.post('/hello', function(req, res) {
+  res.send('Hello, ' + req.body.name + ' from a POST endpoint!');
+})
 
 app.listen(8000, function() {
   console.log('Server is listening on port 8000');
