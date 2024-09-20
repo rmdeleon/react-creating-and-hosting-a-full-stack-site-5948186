db9d6e18309fda81a5900528fdf7ae3389b5356c
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-20 20:09:12 +0000

    Added a login logout button to the nav bar

--- front-end/src/NavBar.jsx
+++ front-end/src/NavBar.jsx
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { getAuth, signOut } from 'firebase/auth';
 
 export default function NavBar() {
+  const isLoggedIn = false;
+  // const email = 'shaun@gmail.com';
+
+  const navigate = useNavigate();
+
   return (
     <nav>
       <ul>
@@ -12,6 +19,16 @@ export default function NavBar() {
         </li>
         <li>
           <Link to='/articles'>Articles</Link>
+        </li>
+        {isLoggedIn && (
+          <li style={{ color: 'white' }}>
+            Logged in as {email}
+          </li>
+        )}
+        <li>
+          {isLoggedIn
+            ? <button onClick={() => signOut(getAuth())}>Sign Out</button>
+            : <button onClick={() => navigate('/login')}>Sign In</button>}
         </li>
       </ul>
     </nav>
