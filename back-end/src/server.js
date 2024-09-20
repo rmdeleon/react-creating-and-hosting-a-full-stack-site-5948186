23250a3adfa6a830b9827e052358722b6dc3682c
Shaun Wassell <shaun.p.wassell@gmail.com>
2024-09-20 23:47:24 +0000

    Modified some logic

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -23,7 +23,7 @@ app.use(express.json());
 let db;
 
 async function connectToDB() {
-  const uri = process.env.MONGODB_USERNAME
+  const uri = !process.env.MONGODB_USERNAME
     ? 'mongodb://127.0.0.1:27017'
     : `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.zplzm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
