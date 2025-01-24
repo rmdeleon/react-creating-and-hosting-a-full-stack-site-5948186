bbe60c9e6ffc532fc1a12537f2019d7b7453c70d
rmdeleon <rmdeleon@gmail.com>
2025-01-24 17:32:09 +0000

    changes until 01_06

---
+++ front-end/src/pages/ArticlePage.jsx
@@ -0,0 +1,5 @@
+export default function ArticlePage() {
+  return(
+    <h1>This is the article page!</h1>
+  );
+}
---
+++ front-end/src/pages/ArticlesList.jsx
@@ -0,0 +1,5 @@
+export default function ArticlesList() {
+  return(
+    <h1>This is the articles list page!</h1>
+  );
+}
